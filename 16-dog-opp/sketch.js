93947dea0f6368e8faa84a757675014418bec108
H0P34U1 <146876269+H0P34U1@users.noreply.github.com>
2023-11-14 11:57:43 -0600

    dog opp demo

--- 16-dog-opp/sketch.js
+++ 16-dog-opp/sketch.js
@@ -1,22 +1,26 @@
 // Project Title
 
 class Dog {
-  constructor() {
-    this.color = "black";
-    this.breed = "poodle";
-    this.age = 13;
-    this.size = "smallish";
+  constructor(name, color, breed, age, size) {
+    this.name = name;
+    this.color = color;
+    this.breed = breed;
+    this.age = age;
+    this.size = size;
   }
 
   bark() {
-    console.log("Arf!");
+    console.log("Arf! says " + this.name);
   }
 }
 
-let spot = new Dog();
+let spot = new Dog("Spot", "black", "poodle", 3, "smallish");
+let rover = new Dog("Rover", "white", "German Shepherd", 5, "big");
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  spot.bark();
+  rover.bark();
 }
 
 function draw() {
